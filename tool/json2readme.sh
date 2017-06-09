40432ca8e46b445d97dd7f42efb4ca69a86b208c
Rex Tsou <akccakccwww@gmail.com>
2017-06-09 22:54:15 +0800

    Close #13, formatter files

--- tool/json2readme.sh
+++ tool/json2readme.sh
@@ -11,13 +11,13 @@ echo '# [Front-end Navigation](https://akccakcctw.github.io/frontend-navigation/
 echo '' >> README.md
 
 for (( i=0; i<$jsonLen; i++ )); do
-	
+
 	echo -ne "${revertColor}[ $i/$jsonLen ]${noColor}"\\r # show progress
-	
+
 	key=$(cat js/data.json | jq "keys_unsorted[$i]") # category with quote
 	keyOutput=$(cat js/data.json | jq "keys_unsorted[$i]" -r) # category without quote
 	value=$(cat js/data.json | jq ".$key[0]" -r) # category in Chinese
-	
+
 	echo \#\# [$value\($keyOutput\)]\(https://akccakcctw.github.io/frontend-navigation/#$keyOutput\) >> README.md
 	echo '' >> README.md
 
