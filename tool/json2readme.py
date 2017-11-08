3a06cafa5ba7ed5601b1282a73213bdc7b646daf
Rex Tsou <akccakccwww@gmail.com>
2017-11-08 13:36:41 +0800

    Add tool/json2readme.py(faster)

---
+++ tool/json2readme.py
@@ -0,0 +1,36 @@
+""" convert raw data(js/data.json) to README.md """
+import json
+import time
+import sys
+import codecs
+from colorama import init, Fore, Back
+
+init()  # colorama
+
+
+def progress(idx, length):
+    """ show progress """
+    time.sleep(0.01)
+    sys.stdout.write(Fore.WHITE + Back.CYAN +
+                     '\r[ {0} / {1} ]'.format(idx, length))
+    sys.stdout.flush()
+
+
+with open('js/data.json', encoding='utf-8') as rawData:
+    DATA = json.load(rawData)
+
+README = codecs.open('README.md', 'w', 'utf-8')
+
+README.write(
+    '# [Front-end Navigation](https://akccakcctw.github.io/frontend-navigation/)\n\n')
+for i, t in enumerate(DATA):  # t: type
+    progress(i, len(DATA))
+    README.write('## [{0}({1})]({2}{1})\n'.format(
+        t['name'], t['type'], 'https://akccakcctw.github.io/frontend-navigation/#'))
+    for l in t['data']:  # l: list
+        README.write(
+            '- [{0}]({1}) - {2}\n'.format(l['name'], l['url'], l['desc']))
+    README.write('\n')
+
+README.close()
+sys.stdout.write('\r[___done___]\n')
